3df0b2b0708032e00d7a8fc2466b4120220928ee
Kevin Labtani <kevinlabtani@gmail.com>
2019-11-28 11:39:17 +0100

    cleanup forms ex6

--- forms/index.php
+++ forms/index.php
@@ -22,11 +22,7 @@
                     if (empty($_POST['firstName']) || empty($_POST['lastName'])) {
                         include 'form.php';
                     } else {
-                        echo '<h5 class="pink-text center-align">'.$_POST['title'].' '.$_POST['firstName'].' '.$_POST['lastName'].'</h5>';
-                        echo '
-                        <div class="center">
-                            <a href="index.php" class="btn btn-small pink center">Back to the form</a>
-                        </div>';
+                        include 'result.php';
                     }
                 ?>
             </div>
--- forms/result.php
+++ forms/result.php
@@ -1,7 +1,7 @@
 
-<h5>your prefered title is: <?php echo htmlspecialchars($_POST['title']); ?></h5>
-<h5>your first name is: <?php echo htmlspecialchars($_POST['firstName']); ?></h5>
-<h5>your last name is: <?php echo htmlspecialchars($_POST['lastName']); ?></h5>
+<?php
+echo '<h5 class="pink-text center-align">'.$_POST['title'].' '.$_POST['firstName'].' '.$_POST['lastName'].'</h5>';
+?>
 <div class="center">
     <a href="index.php" class="btn btn-small pink center">Back to the form</a>
 </div>
