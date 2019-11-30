474c4f51992f1fb8ad54a1ba650cc88a0dc56a40
Kevin Labtani <kevinlabtani@gmail.com>
2019-11-30 21:49:35 +0100

    remove jquery from materialize, init selector with vanilla js instead

--- forms/index.php
+++ forms/index.php
@@ -51,15 +51,12 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 
-    <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous"
-    ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
-        $(document).ready(function() {
-            $("select").formSelect();
+        document.addEventListener('DOMContentLoaded', function() {
+            let elems = document.querySelectorAll('select');
+            let options = document.querySelectorAll('option');
+            let instances = M.FormSelect.init(elems, options);
         });
     </script>
 </body>
